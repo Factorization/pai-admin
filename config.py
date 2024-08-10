4e1c62b8aa2ffb9ef5e7d29cce09ad211ad73b05
Jeffrey Kraemer <Factorization@users.noreply.github.com>
2024-08-10 09:51:33 -0700

    Fixed True/False checks

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config:
     SITE_NAME = "PAI Admin"
     DEFAULT_TITLE = "PAI Admin"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
+    LOG_TO_STDOUT = True if os.environ.get("LOG_TO_STDOUT", "True").lower() == "true" else False
     SUPPORTED_FILE_EXTENSIONS = ["pdf", "doc", "docx", "txt"]
     DATA_DIR = os.environ.get("DATA_DIR")
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024
-    DELETE_FILES_ENABLED = os.environ.get("DELETE_FILES_ENABLED")
+    DELETE_FILES_ENABLED = True if os.environ.get("DELETE_FILES_ENABLED", "False").lower() == "true" else False
