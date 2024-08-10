f40b9555dfc8e7c80324370902ae577e832580e3
Jeffrey Kraemer <Factorization@users.noreply.github.com>
2024-08-09 22:41:13 -0700

    Updated model for file upload

--- app/views/main/view.py
+++ app/views/main/view.py
@@ -16,7 +16,7 @@ from werkzeug.utils import secure_filename
 
 from app import db
 from app.bin.utils import log_request
-from app.models import File
+from app.models import File, get_all_files
 from app.viewmodels.main.main_viewmodel import MainViewModel
 from app.views.main import bp
 from app.views.main.forms import UploadForm
@@ -37,7 +37,7 @@ def index():
     vm = MainViewModel()
     form = UploadForm
 
-    return render_template("main/main_datatable.html", form=form, **vm.to_dict())
+    return render_template("main/main.html", form=form, **vm.to_dict())
 
 
 @bp.route("/file/<int:id>/delete", methods=["Delete"])
@@ -50,23 +50,38 @@ def delete_file(id):
     return ""
 
 
+@bp.route("/upload", methods=["GET"])
+@login_required
+def upload_modal():
+    log_request()
+    form = UploadForm
+
+    return render_template("main/_partials/upload.html", form=form)
+
+
 @bp.route("/upload", methods=["POST"])
 @login_required
 def upload_files():
+    log_request()
     supported_extensions = {
         f".{extension.removeprefix('.')}"
         for extension in current_app.config["SUPPORTED_FILE_EXTENSIONS"]
     }
     uploaded_files = request.files.getlist("file")
-    print(upload_files)
+    existing_files_names = [file.name for file in get_all_files()]
     for file in uploaded_files:
-        print(file)
         filename = secure_filename(file.filename)  # type: ignore
         if filename != "":
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in supported_extensions:
-                flash(f"Unsupported file type for file '{filename}'.", "danger")
+                flash(f"Error: Unsupported file type for file '{filename}'.", "danger")
+                continue
+            if filename in existing_files_names:
+                flash(
+                    f"File '{filename}' already exists in directory. Please delete the existing file if you want to replace it.",
+                    "warning",
+                )
                 continue
             file.save(os.path.join(current_app.config["DATA_DIR"], filename))
-            flash(f"Success: File '{filename}' uploaded.", "success")
+            flash(f"File '{filename}' uploaded successfully.", "success")
     return redirect(url_for("main.index"))
